817a80cc56d59bda6b14a26a9610a425838a25fd
Kalin Staykov <k.t.staykov@gmail.com>
2023-07-02 16:25:05 +0300

    refactor

--- main.go
+++ main.go
@@ -21,6 +21,16 @@ func ioReader(file string) io.ReaderAt {
 	return r
 }
 
+// mapSection returns 10 bytes hex string for a []byte section
+func mapSection(b []byte) string {
+	return hex.EncodeToString(b[0:10])
+}
+
+// noZeroes returns true if there are no zeros in the string
+func noZeros(s string) bool {
+	return (strings.Count(s, "0") <= 1)
+}
+
 func main() {
 
 	if len(os.Args) < 2 {
@@ -41,34 +51,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	var arch string
-	switch _elf.Class.String() {
-	case "ELFCLASS64":
-		arch = "64 bits"
-	case "ELFCLASS32":
-		arch = "32 bits"
-	}
-
-	var mach string
-	switch _elf.Machine.String() {
-	case "EM_AARCH64":
-		mach = "ARM64"
-	case "EM_386":
-		mach = "x86"
-	case "EM_X86_64":
-		mach = "x86_64"
-	}
-
-	// Print info about the file
-	fmt.Printf("File Header: ")
-	fmt.Println(_elf.FileHeader)
-	fmt.Printf("ELF Class: %s\n", arch)
-	fmt.Printf("Machine: %s\n", mach)
-	fmt.Printf("ELF Type: %s\n", _elf.Type)
-	fmt.Printf("ELF Data: %s\n", _elf.Data)
-	fmt.Printf("Entry Point: %d\n", _elf.Entry)
-	fmt.Printf("Section Addresses: %v\n", _elf.Sections)
-
 	// Process sections
 	hexCollection := []string{}
 	for _, v := range _elf.Sections {
@@ -79,7 +61,7 @@ func main() {
 		}
 
 		if len(b) > 10 {
-			sectionEntry := MapSection(b)
+			sectionEntry := mapSection(b)
 			if noZeros(sectionEntry) {
 				hexCollection = append(hexCollection, sectionEntry)
 			}
@@ -88,14 +70,4 @@ func main() {
 
 	fmt.Printf("hexCollection: %v\n", hexCollection)
 
-}
-
-// MapSection returns 10 bytes hex string for a []byte section
-func MapSection(b []byte) string {
-	return hex.EncodeToString(b[0:10])
-}
-
-// noZeroes returns true if there are no zeros in the string
-func noZeros(s string) bool {
-	return (strings.Count(s, "0") <= 1)
 }
